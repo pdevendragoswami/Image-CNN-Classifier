f52033a0a3a3cc60e8e237317c1b8e49d9e022c5
Devendra Goswami <pdevendragoswami@gmail.com>
2023-12-05 19:39:16 +0530

    setup file updated

--- setup.py
+++ setup.py
@@ -2,25 +2,24 @@ import setuptools
 
 __version__ = '0.0.1'
 
-REPO_NAME = 'CNNClassifier'
-AUTHOR_NAME = 'Devendra Goswami'
-SRC_REPO = "CNNClassifier"
-AUTHOR_Email = 'pdevendragoswami@gmail.com'
+Repo_Name = 'Image-CNN-Classifier'
+Author_Name = 'Devendra Goswami'
+Src_Repo = 'Image-CNN-Classifier'
+Author_Email = 'pdevendragoswami@gmail.com'
 
-with open('README.md', 'r') as f:
-    long_description = f.read()
-
+with open('README.md','r') as file:
+    long_description = file.read()
 
 setuptools.setup(
-    name=SRC_REPO,
-    version=__version__,
-    author=AUTHOR_NAME,
-    author_email=AUTHOR_Email,
-    description= 'this is an image classification project',
-    long_description=long_description,
-    long_description_content = "text/markdown",
-    url = f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}",
-    project_url ={"Bug Tracker":f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}"},
+    name = Src_Repo,
+    version = __version__,
+    author = Author_Name,
+    author_email = Author_Email,
+    description = 'End to end Image Classification',
+    long_description = long_description,
+    long_description_content = 'text/markdown',
+    url = f"https://github.com/{Author_Name}/{Repo_Name}",
+    project_url = {"Bug Tracker":f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}"},
     package_dir = {"":"src"},
     packages=setuptools.find_package(where='src')
 
